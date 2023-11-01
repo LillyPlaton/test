7acde9d5e889c3e6bd5a802b3aae89fa9adc7c96
Liliana Platon <l.platon@web.de>
2023-11-01 15:48:43 +0100

    make addition calculator

--- src/Hello.java
+++ src/Hello.java
@@ -1,6 +1,12 @@
+import java.util.Scanner;
+
 public class Hello {
 
+
   public static void main(String[] args) {
-    System.out.println("Hello word");
+    Scanner scanner = new Scanner(System.in);
+    int a = scanner.nextInt();
+    int b = scanner.nextInt();
+    System.out.println(a + b);
   }
 }
