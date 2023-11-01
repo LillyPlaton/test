3c6db7cfee84b721e9630eaecd4edf58e1fb9541
Liliana Platon <l.platon@web.de>
2023-11-01 19:10:57 +0100

    add some code

---
+++ src/Main01.java
@@ -0,0 +1,27 @@
+
+
+import java.util.Scanner;
+
+public class Main01 {
+
+  public static double discriminant(double a, double b, double c) {
+    return b * b - 4 * a * c;
+  }
+
+  public static void solveAndPrintRoots(double a, double b, double c) {
+    double d = discriminant(a, b, c);
+    double x1 = (-b + Math.sqrt(d)) / (2 * a);
+    double x2 = (-b - Math.sqrt(d)) /(2 * a);
+    System.out.println("x1 = " + x1 + ", x2 = " + x2);
+  }
+
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    double a = scanner.nextDouble();
+    double b = scanner.nextDouble();
+    double c = scanner.nextByte();
+    solveAndPrintRoots(a, b, c);
+  }
+}
+
+
